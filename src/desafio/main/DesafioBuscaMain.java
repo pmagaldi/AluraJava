292709bfbc36468ea70c7a31524bbdcdae08c4ed
Paulo Magaldi <avanade.pmagaldi@voeazul.com.br>
2024-09-26 12:59:41 -0300

    terminado o desafio da aula 1

--- src/desafio/main/DesafioBuscaMain.java
+++ src/desafio/main/DesafioBuscaMain.java
@@ -9,10 +9,19 @@ import java.util.Scanner;
 
 public class DesafioBuscaMain {
     public static void main(String[] args) throws IOException, InterruptedException {
-        /* TODO - Crie um programa em Java que utilize as classes HttpClient, HttpRequest e HttpResponse
-         * para fazer uma consulta à API do Google Books. Solicite ao usuário que insira o título de um livro, 
-         * e exiba as informações disponíveis sobre o livro retornado pela API.
-        */
+        //executar primeira task - comente para não rodar
+        //primeiroTask();
+        //executar segundo task - comente para não rodar
+        //segundoTask();
+        //executar terceiro task - comente para não rodar
+        terceiroTask();
+    }
+
+    /* TODO - Crie um programa em Java que utilize as classes HttpClient, HttpRequest e HttpResponse
+        * para fazer uma consulta à API do Google Books. Solicite ao usuário que insira o título de um livro, 
+        * e exiba as informações disponíveis sobre o livro retornado pela API.
+    */
+    public static void primeiroTask() throws IOException , InterruptedException{
         Scanner teclado = new Scanner(System.in);
         System.out.println("Digite um nome de um livro que deseja buscar: ");
         String nomeLivro = teclado.nextLine();
@@ -26,12 +35,39 @@ public class DesafioBuscaMain {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         System.out.println("Sobre o livro " + nomeLivro + ": "+response.body());
         teclado.close();
-        /*TODO - Crie um programa que faça a mesma coisa que o de cima, contudo com a API CoinGecko
-         * e exiba a contação atual de uma criptomoeda escolhida pelo usuário
-        */
+    }
 
-        /*TODO - Crie um programa que faça consulta à API do TheMealDB utilizando as classes acima.
+    /*TODO - Crie um programa que faça a mesma coisa que o de cima, contudo com a API CoinGecko
+         * e exiba a contação atual de uma criptomoeda escolhida pelo usuário
+    */
+    public static void segundoTask() throws IOException, InterruptedException{
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome da coin que deseja pesquisar: ");
+        String coin = teclado.nextLine();
+        String uri_final = "https://pro-api.coingecko.com/api/v3/coins/" + coin + "?x_cg_pro_api_key="; // Não criei key pois é pago
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                        .uri(URI.create(uri_final))
+                        .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Sobre o Coin " + coin + ": " + response.body());
+        teclado.close();
+    }
+
+    /*TODO - Crie um programa que faça consulta à API do TheMealDB utilizando as classes acima.
          * Solicite ao usuário que insira o nome de uma receita e exiba as informações disponíveis sobre a mesma.
-         */
+    */
+    public static void terceiroTask() throws IOException, InterruptedException{
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite o nome da receita: ");
+        String nomeReceita = teclado.nextLine();
+        String uri_final = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + nomeReceita;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                            .uri(URI.create(uri_final))
+                            .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println("Sobre a receita " + nomeReceita + ": " + response.body());
+        teclado.close();
     }
 }
